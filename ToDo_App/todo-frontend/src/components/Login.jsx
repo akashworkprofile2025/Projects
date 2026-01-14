3a6106b15df9547853adfd20b4d1daecfda1d30b
akashworkprofile2025 <akashbiswas.officialmail2025@gmail.com>
2026-01-14 13:14:51 +0530

    react

--- ToDo_App/todo-frontend/src/components/Login.jsx
+++ ToDo_App/todo-frontend/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import api from "../api/axios";
 import { useNavigate } from "react-router-dom";
-import Register from "./Register";
 
 const Login = () => {
   const [username, setUsername] = useState("");
